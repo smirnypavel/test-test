1990050a178fa0e38a976af40e1547d43d118cd3
smirnypavel <smirnypavlik@gmail.com>
2022-11-25 18:06:34 +0200

    form
    form

---
+++ js/form.js
@@ -0,0 +1,9 @@
+// document.addEventListener('DOMContentLoaded', function (){
+//     const form = document.getElementById('form');
+//     form.addEventListener('submit', formSend);
+
+//     async function formSend(e) {
+//         e.preventDefault();
+
+//     }
+// })

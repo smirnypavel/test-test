78b6dd294625ca01795707c7e8cf47ac5dbccfdd
smirnypavel <smirnypavlik@gmail.com>
2022-11-25 18:35:46 +0200

    bagfix
    form-ukr

--- js/form.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function (){
-//     const form = document.getElementById('form');
-//     form.addEventListener('submit', formSend);
-
-//     async function formSend(e) {
-//         e.preventDefault();
-
-//     }
-// })
